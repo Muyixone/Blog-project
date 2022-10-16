c5c0324ed177e03723f5b70abbe9834670ec0f58
Osamuyi <muyixone@yahoo.com>
2022-10-16 11:23:40 +0100

    Storing BlogPosts to MongoDB database

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const ejs = require('ejs');
+
+const BlogPost = require('./models/BlogPost');
 
 const path = require('path');
 
@@ -8,27 +11,48 @@ const PORT = 7000;
 
 const app = new express();
 
-app.set('view engine', 'ejs')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+  const blogposts = await BlogPost.find({});
+  res.render('index', {
+    blogposts,
+  });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+  res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+  res.render('contact');
 });
 
-app.get('/post', (req, res) => {
-    res.render('post');
+app.get('/post/:id', async (req, res) => {
+  const { id } = req.params;
+  const blogpost = await BlogPost.findById(id);
+  res.render('post', {
+    blogpost,
+  });
 });
 
+app.get('/posts/new', (req, res) => {
+  res.render('create');
+});
+
+app.post('/posts/store', async (req, res) => {
+  await BlogPost.create(req.body);
+  res.redirect('/');
+});
+
+// CONNECTING TO DB
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true });
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})
+  console.log(`App listening on port ${PORT}`);
+});
